7f42f376dd712c5b368d1e4475bc0ea31f110fb9
Kun Zhang <kunzhang1110@gmail.com>
2018-10-30 13:53:54 +1100

    orgnised files

--- web_server/config.js
+++
@@ -1,9 +0,0 @@
-const env = process.env;  //process.env is defined in nodejs process module
-
-export default { //default export of this module
-  port: env.PORT || 3000,
-  host: env.HOST || '0.0.0.0',
-  get serverUrl(){//ES6 getter
-    return `http://${this.host}:${this.port}`;
-  }
-};
